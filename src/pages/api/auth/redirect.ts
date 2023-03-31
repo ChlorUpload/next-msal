075a2424c55d41d8783925e9c4c50068bf3424ab
ChlorUpload <chlor.upload@gmail.com>
2023-03-31 15:20:48 +0900

    see redirect

--- src/pages/api/auth/redirect.ts
+++ src/pages/api/auth/redirect.ts
@@ -7,27 +7,34 @@ import { NextApiRequest, NextApiResponse } from "next";
 async function redirect(req: NextApiRequest, res: NextApiResponse) {
   console.log("SESSION!!!", req.session, req.body, req.cookies);
 
-  const authCodeRequest = {
-    redirectUri,
-    scopes: [scope],
-    code: req.body.code,
-    codeVerifier: req.session.pkceCodes.verifier,
-  };
+  res.send({
+    session: req.session,
+    body: req.body,
+    cookies: req.cookies,
+  });
 
-  try {
-    const tokenResponse = await pca.acquireTokenByCode(authCodeRequest);
-    const state = tokenResponse.state;
-    req.session.accessToken = tokenResponse.accessToken;
-    req.session.idToken = tokenResponse.idToken;
-    req.session.account = tokenResponse.account;
-    req.session.isAuthenticated = true;
-    await req.session.save();
+  // const authCodeRequest = {
+  //   redirectUri,
+  //   scopes: [scope],
+  //   code: req.body.code,
+  //   codeVerifier: req.session.pkceCodes.verifier,
+  // };
 
-    res.redirect("/");
-  } catch (error) {
-    console.error(error);
-    res.end();
-  }
+  // try {
+  //   const tokenResponse = await pca.acquireTokenByCode(authCodeRequest);
+  //   const state = tokenResponse.state;
+  //   console.log(state);
+  //   req.session.accessToken = tokenResponse.accessToken;
+  //   req.session.idToken = tokenResponse.idToken;
+  //   req.session.account = tokenResponse.account;
+  //   req.session.isAuthenticated = true;
+  //   await req.session.save();
+
+  //   res.redirect("/");
+  // } catch (error) {
+  //   console.error(error);
+  //   res.end();
+  // }
 }
 
 export default withIronSessionApiRoute(redirect, sessionOptions);
