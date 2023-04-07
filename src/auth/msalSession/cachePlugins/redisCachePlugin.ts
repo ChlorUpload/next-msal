7a0e0d84287c20b01a22dc1a8a6c21b2975e3ed1
ChlorUpload <chlor.upload@gmail.com>
2023-04-07 15:22:52 +0900

    feat: Update redis-cache plugin for azure-cache

--- src/auth/msalSession/cachePlugins/redisCachePlugin.ts
+++ src/auth/msalSession/cachePlugins/redisCachePlugin.ts
@@ -1,19 +1,26 @@
 import { ICachePlugin, TokenCacheContext } from "@azure/msal-node";
 import { createClient } from "redis";
 
-const redisKey = "msal-cache";
+const redisMsalKey = process.env.REDIS_MSAL_KEY as string;
+const redisHostName = process.env.REDIS_HOST_NAME as string;
+const redisPassword = process.env.REDIS_PASSWORD as string;
 
-const client = createClient();
+if (!redisMsalKey || !redisHostName || !redisPassword)
+  throw new Error("env not set");
+
+const client = createClient({
+  url: `rediss://${redisHostName}:6380`,
+  password: redisPassword,
+});
 type ClientType = typeof client;
-client.on("error", (err) => console.log("Redis Client Error", err));
-
+client.on("error", (err) => console.error("Redis Client Error", err));
 export class RedisCachePlugin implements ICachePlugin {
   client: ClientType;
   constructor(client: ClientType) {
     this.client = client;
   }
   async beforeCacheAccess(cacheContext: TokenCacheContext): Promise<void> {
-    const cacheData = await this.client.get(redisKey);
+    const cacheData = await this.client.get(redisMsalKey);
     if (cacheData === null) return;
 
     cacheContext.tokenCache.deserialize(cacheData);
@@ -22,7 +29,7 @@ export class RedisCachePlugin implements ICachePlugin {
   async afterCacheAccess(cacheContext: TokenCacheContext): Promise<void> {
     if (cacheContext.cacheHasChanged) {
       const serialized = cacheContext.tokenCache.serialize();
-      await this.client.set(redisKey, serialized);
+      await this.client.set(redisMsalKey, serialized);
     }
   }
 }
